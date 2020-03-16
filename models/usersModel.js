92b33a96a7f241b362c110a1f5ff8171fa8dae3f
jamariod <j25ghs99@aol.com>
2020-03-16 11:12:43 -0400

    Added changes to usersModel.js

--- models/usersModel.js
+++ models/usersModel.js
@@ -10,11 +10,36 @@ class User {
     this.password = password;
   }
   async addUser() {
-    console.log("adding user", this.name);
+    try {
+      const response = await db.one(
+        "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id;",
+        [this.name, this.email, this.password]
+      );
+      return response;
+    } catch (error) {
+      console.error("ERROR", error);
+      return error;
+    }
   }
 
   async loginUser() {
-    console.log("loging in user");
+    try {
+      const response = await db.one(
+        `SELECT id, name, password FROM users WHERE email = $1;`,
+        [this.email]
+      );
+      console.log("response is", response);
+      const isValid = this.checkpassword(response.password);
+      if (!!isValid) {
+        const { id, name } = response;
+        return { isValid, id, name };
+      } else {
+        return { isValid };
+      }
+    } catch (error) {
+      console.error("ERROR", error);
+      return error;
+    }
   }
 }
 module.exports = User;
