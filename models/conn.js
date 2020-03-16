e15f7d384d8962b838cd53e64fbb701540024dd9
jamariod <j25ghs99@aol.com>
2020-03-16 10:42:05 -0400

    Updated conn.js file

--- models/conn.js
+++ models/conn.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
 const options = {
-  host: "localhost",
-  database: "job",
-  user: process.env["DB_USER"],
-  password: process.env["DB_PW"]
+  host: "drona.db.elephantsql.com",
+  database: "tkbowobo",
+  user: process.env["tkbowobo"],
+  password: process.env["K2YWJMmSYD4i5LL-13t_uWu-yRzRPFhE"]
 };
 
 const pgp = require("pg-promise")({
